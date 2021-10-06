b16fd6c6c07ccde3e303dde11be0c4b470044078
voyageyang <583992171@qq.com>
2021-10-06 17:49:07 +0800

    finished MIT6.092 lec1 assignments

---
+++ Java/MIT6_092/src/lec01/assignments/GravityCalculatorPart01.java
@@ -0,0 +1,15 @@
+package lec01.assignments;
+
+public class GravityCalculatorPart01 {
+    public static void main(String[] arguments) {
+        double gravity = -9.81; // Earth's gravity in m/s^2
+        double initialVelocity = 0.0;
+        double fallingTime = 10.0;
+        double initialPosition = 0.0;
+        double finalPosition = 0.0;
+        System.out.println("The object's position after " + fallingTime +
+                " seconds is " + finalPosition + " m.");
+
+        // output of this program : The object's position after 10.0 seconds is 0.0 m.
+    }
+}
