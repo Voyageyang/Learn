9b3952daa83f076fbb6ceedf82685a3a64e4c639
voyageyang <583992171@qq.com>
2021-10-06 19:01:08 +0800

    improve solution in MIT6.092 lec2 assignments

--- Java/MIT6_092/src/lec01/lec02/assignments/FooCorporation.java
+++ Java/MIT6_092/src/lec01/lec02/assignments/FooCorporation.java
@@ -9,17 +9,19 @@ public class FooCorporation {
     public static void getPaid(double basePay, double workHour) {
         System.out.println("Your base pay is $" + basePay + ".");
         System.out.println("Your working hour is " + workHour + " hours.\n");
-        if(basePay < 8) System.out.println("Base pay so little!!!");
-        if(workHour > 60) System.out.println("Work too long!!!");
-        boolean flag = (basePay >= 8) && (workHour < 60);
-        if(flag) {
-            double getPay = 0;
+
+        if(basePay < 8) {
+            System.out.println("Base pay so little!!!");
+        } else if(workHour > 60) {
+            System.out.println("Work too long!!!");
+        } else {
             if(basePay <= 40) {
                 System.out.println("Your total pay is $" + workHour * basePay + ".");
             } else {
                 System.out.println("Your total pay is $" + 40 * basePay + (workHour - 40) * basePay * 1.5 + ".");
             }
         }
+
         System.out.println("=====================================================");
         System.out.println("\n");
     }
