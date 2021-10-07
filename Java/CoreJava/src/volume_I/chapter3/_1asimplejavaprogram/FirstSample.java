58691c4c3db774f9d13e0675c2cc2995cc939325
voyageyang <583992171@qq.com>
2021-10-07 09:27:08 +0800

    Core Java

---
+++ Java/CoreJava/src/volume_I/chapter3/_1asimplejavaprogram/FirstSample.java
@@ -0,0 +1,7 @@
+package volume_I.chapter3._1asimplejavaprogram;
+
+public class FirstSample {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello World!'");
+    }
+}
